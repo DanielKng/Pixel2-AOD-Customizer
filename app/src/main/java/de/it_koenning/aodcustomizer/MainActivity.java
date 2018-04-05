57e7a8cb495cee20ccba743e3c8b8041008edfca
DanielKng <koenning.daniel@gmail.com>
2018-04-05 15:21:18 +0200

    Small bug-fixing.

--- app/src/main/java/de/it_koenning/aodcustomizer/MainActivity.java
+++ app/src/main/java/de/it_koenning/aodcustomizer/MainActivity.java
@@ -1,27 +1,32 @@
 package de.it_koenning.aodcustomizer;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        SharedPreferences pref = getSharedPreferences("ActivityPREF", Context.MODE_PRIVATE);
-        if(pref.getBoolean("wasIntroShown", false)){
+        SharedPreferences getPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isFirstRun = getPrefs.getBoolean("firstRun", true);
+
+        if (isFirstRun) {
+            //  Make a new preferences editor
+            SharedPreferences.Editor e = getPrefs.edit();
+
+            //  Edit preference to make it false because we don't want this to run again
+            e.putBoolean("firstRun", false);
+
+            //  Apply changes
+            e.apply();
+
             Intent intent = new Intent(this, MainIntroActivity.class);
             startActivity(intent);
-            finish();
-        } else {
-            SharedPreferences.Editor ed = pref.edit();
-            ed.putBoolean("wasIntroShown", true);
-            ed.commit();
         }
+        setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/de/it_koenning/aodcustomizer/MainIntroActivity.java
+++ app/src/main/java/de/it_koenning/aodcustomizer/MainIntroActivity.java
@@ -1,5 +1,7 @@
 package de.it_koenning.aodcustomizer;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -22,11 +24,11 @@ public class MainIntroActivity extends AppIntro {
 
         // OPTIONAL METHODS
         // Override bar/separator color.
-        /*setBarColor(Color.parseColor("#3F51B5"));
-        setSeparatorColor(Color.parseColor("#2196F3"));*/
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
 
         // Hide Skip/Done button.
-        showSkipButton(false);
+        showSkipButton(true);
         setProgressButtonEnabled(true);
 
         // Turn vibration on and set intensity.
@@ -38,18 +40,24 @@ public class MainIntroActivity extends AppIntro {
     @Override
     public void onSkipPressed(Fragment currentFragment) {
         super.onSkipPressed(currentFragment);
-        // Do something when users tap on Skip button.
+        StartMainActivity();
     }
 
     @Override
     public void onDonePressed(Fragment currentFragment) {
         super.onDonePressed(currentFragment);
-        // Do something when users tap on Done button.
+        StartMainActivity();
     }
 
     @Override
     public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
         super.onSlideChanged(oldFragment, newFragment);
         // Do something when the slide changes.
+        StartMainActivity();
+    }
+
+    private void StartMainActivity(){
+        Intent startMain = new Intent(MainIntroActivity.this, MainActivity.class);
+        startActivity(startMain);
     }
 }
