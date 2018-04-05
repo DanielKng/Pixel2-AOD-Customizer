dd9ac08a8e20cb98300c1e95046c00fb0629ad29
DanielKng <koenning.daniel@gmail.com>
2018-04-05 16:07:59 +0200

    Tryhard.

--- app/src/main/java/de/it_koenning/aodcustomizer/MainActivity.java
+++ app/src/main/java/de/it_koenning/aodcustomizer/MainActivity.java
@@ -10,23 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        SharedPreferences getPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean isFirstRun = getPrefs.getBoolean("firstRun", true);
-
-        if (isFirstRun) {
-            //  Make a new preferences editor
-            SharedPreferences.Editor e = getPrefs.edit();
-
-            //  Edit preference to make it false because we don't want this to run again
-            e.putBoolean("firstRun", false);
-
-            //  Apply changes
-            e.apply();
-
-            Intent intent = new Intent(this, MainIntroActivity.class);
-            startActivity(intent);
-        }
         setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/de/it_koenning/aodcustomizer/MainIntroActivity.java
+++ app/src/main/java/de/it_koenning/aodcustomizer/MainIntroActivity.java
@@ -1,26 +1,32 @@
 package de.it_koenning.aodcustomizer;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import com.github.paolorotolo.appintro.AppIntro2;
 import de.it_koenning.aodcustomizer.slides.FirstSlide;
+import de.it_koenning.aodcustomizer.slides.SecondSlide;
 
 
 public class MainIntroActivity extends AppIntro2 {
+    //Declaring the slides
     FirstSlide first_slide = new FirstSlide();
+    SecondSlide second_slide = new SecondSlide();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //Hide Status-Bar
         showStatusBar(false);
         //Open up the slides
         addSlide(first_slide);
+        addSlide(second_slide);
 
         // OPTIONAL METHODS
-
         // Hide Skip/Done button.
         showSkipButton(true);
         setProgressButtonEnabled(true);
